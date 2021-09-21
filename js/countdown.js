9e9f75c74dc605c8ae3b4a49271d9969cb15b075
Ket <ketulpatel@Ketuls-MacBook-Pro.local>
2021-09-21 15:15:10 +0100

    countdown function added. setter row replaced

--- js/countdown.js
+++ js/countdown.js
@@ -1,28 +1,73 @@
+var myArray = [
+    {'area':'Long Island Wall', 'stripdate':'Dec 9, 2021', 'resetdate':'10/12', 'timeleft':''},
+    {'area':'Font Back Wall', 'stripdate':'Oct 7, 2021', 'resetdate':'08/10', 'timeleft':'test'},
+    {'area':'Islands Back Wall', 'stripdate':'Oct 21, 2021', 'resetdate':'10/22', 'timeleft':'test'},
+    {'area':'Rhino', 'stripdate':'Sept 28, 2021', 'resetdate':'29/09', 'timeleft':'test'},
+    {'area':'Comp Wall', 'stripdate':'Sept 21, 2021', 'resetdate':'25/09', 'timeleft':''},
+    {'area':'Islands 3+4', 'stripdate':'Nov 18, 2021', 'resetdate':'19/10', 'timeleft':''},
+    {'area':'Font Boulders', 'stripdate':'Oct 26, 2021', 'resetdate':'27/10', 'timeleft':''},
+    {'area':'Islands 1+2', 'stripdate':'Dec 02, 2021', 'resetdate':'03/12', 'timeleft':''},
+    {'area':'Power Tunnel', 'stripdate':'Oct 14, 2021', 'resetdate':'15/10', 'timeleft':''},
+    {'area':'Circuit Board 1-4', 'stripdate':'', 'resetdate':'', 'timeleft':''},
+    {'area':'Auto Belays 1-4', 'stripdate':'', 'resetdate':'', 'timeleft':''},
+    {'area':'Auto Belays 5-7', 'stripdate':'', 'resetdate':'', 'timeleft':''},
+    {'area':'Auto Belays 8-10', 'stripdate':'', 'resetdate':'', 'timeleft':''},
+]
+
+
+
+
 const countdown = () => {
-  // getTime returns the time in milliseconds 
-  const countDate = new Date("Sept 15, 2021 00:00:").getTime();
-  // const countDate = new Date().getDate();
-  const now = new Date().getTime();
-  const gap = countDate - now;
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    for (var i = 0; i < myArray.length; i++) {
+        // getTime returns the time in milliseconds 
+        // const countDate = new Date("Sept 15, 2021 00:00:").getTime();
+        const countDate = new Date(myArray[i].stripdate).getTime();
+        // const countDate = new Date().getDate();
+        const now = new Date().getTime();
+        const gap = countDate - now;
+        
 
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-  document.querySelector(".day").innerText = textDay + 'day ' + textHour + 'h' + textMinute + 'm' + textSecond + 's';
-  document.querySelector(".hour").innerText = textHour;
-  document.querySelector(".minute").innerText = textMinute;
-  document.querySelector(".second").innerText = textSecond;
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) / hour);
+        const textMinute = Math.floor((gap % hour) / minute);
+        const textSecond = Math.floor((gap % minute) / second);
 
-  if(gap < 10000) {
-    
-  }
+        myArray[i].timeleft = textDay + ' days';
+
+        if(gap < 10000 | isNaN(textDay)) {
+            // when timer runs out
+            myArray[i].timeleft = '-';
+        }
+    }
+    buildTable(myArray);
 };
 
-setInterval(countdown, 1000);
+countdown()
+// setInterval(countdown, 1000);
+
+
+// BUILD TABLE
+function buildTable(data){
+    var table = document.getElementById('myTable')
+
+    //table is cleared each time function is called (for setInterval countdown)
+    // table.innerHTML = ''
+    
+    for (var i = 0; i < data.length; i++){
+
+        var row = `<tr>
+                        <td>${data[i].area}</td>
+                        <td>${data[i].stripdate}</td>
+                        <td>${data[i].resetdate}</td>
+                        <td class="time-left">${data[i].timeleft}</td>
+                   </tr>`
+        table.innerHTML += row
+        
+    }
+}
